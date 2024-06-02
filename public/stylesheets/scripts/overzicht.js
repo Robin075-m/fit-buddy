1723bed402300af547cd13f05baf54403f2694f9
AidanMW22 <150928246+AidanMW22@users.noreply.github.com>
2024-06-02 22:27:32 +0200

    Styling filter met filter buttons aan de zijkant, h2 toegevoegd.

--- public/stylesheets/scripts/overzicht.js
+++ public/stylesheets/scripts/overzicht.js
@@ -1,13 +1,34 @@
 
 
-console.log ('Hallo')
+console.log('Hallo')
 
 
 //filter
 
 let options = {
-    valueNames: [ 'naam' ],
-  };
-  
-  let userList = new List('nameList', options);
-  
+  valueNames: ['naam'],
+};
+
+let userList = new List('mainSection', options);
+
+
+
+// Pill code voor wanneer het van kleur verandert
+document.querySelectorAll('.pill').forEach(pill => {
+  pill.addEventListener("click", () => pill.classList.toggle("pill--selected"))
+})
+
+
+
+
+
+// De kaarten geven een animatie wanneer ze worden geresized
+window.addEventListener('resize', function () {
+  const grid = document.querySelector('.kaarten');
+  grid.style.transition = 'transform 0.5s ease-out';
+  grid.style.transform = 'scale(0.95)'; // Scale down when resizing
+
+  setTimeout(() => {
+    grid.style.transform = 'scale(1)'; // Gaat weer terug naar zijn originele vorm
+  }, 500);
+});
